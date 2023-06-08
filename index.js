c6dee73c71693d7656a85859fc366c31a7dab87d
unknown <abu040810@gmail.com>
2023-06-09 00:07:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+// "use strict";
+
+// // function types in Javascript
+
+// // type 1   function decloration
+
+// //  function function_name(a,b,c){
+// //    console.log('Hello World');
+// // //    console.log(a,b,c);
+// // // }
+
+// // function_name(11,12,13)
+
+
+// 1 - masala
+
+// function daraja(n) {
+//     console.log(n ** 3);
+
+// }
+// daraja(3)
+
+// 2 - masala
+
+// function daraja(a, b) {
+//     console.log(a + b);
+// }
+// daraja(3, 4);
+
+// 3 - masala
+
+// function daraja(a, b) {
+//     console.log(a - b);
+// }
+// daraja(3, 4);
+
+// 4 - masala
+
+// function daraja(a) {
+//     console.log(a ** 2);
+//     console.log(a ** 3);
+//     console.log(a ** 4);
+// }
+// daraja(3);
+
+// 5 - masala
+
+// function toq(n) {
+//     for (let i = 0; i <= n; i++)
+//         if (i % 2 == 1) {
+//             console.log(i);
+//         }
+
+// }
+// toq(189)
+
+
+// 6 - masala
+
+// function juft(n) {
+//     for (let i = 0; i <= n; i++)
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         }
+// }
+// juft(189);
+
+// 8 - masala
+
+// function b(a) {
+//     for (let i = 1; i <= a; i++) {
+//         if (a % i === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// let k = +prompt("N:")
+
+// b(k)
+
+// 7 - masala
+
+// function Increment(son) 
+// {
+//     console.log(son - 1);
+// }
+
+// function Dikrement(son) {
+//     console.log(son)
+// }
+// let som = +prompt("SON")
+// let b = +prompt("Increment(1) Dikrement (0)")
+
+// if (a === 1) {
+//     Increment(son)
+// } else if (a === 0) {
+//     Dikrement(son)
+// } else {
+//     console.log("SIZ faqat 0 va 1 kirita olasiz")
+// }
+
+// 9 - masala
+
+// function calc(son, son2, a) {
+//     if (a === '*') {
+//         console.log(son * son2)
+//     } else if (a === '+') {
+//         console.log(son + son2)
+//     } else if (a === '-') {
+//         console.log(son - son2)
+//     } else if (a === '/') {
+//         console.log(son / son2)
+//     }
+// }
+// let son = +prompt("SON1")
+// let son2 = +prompt("SON2")
+// let a = prompt("amal")
+
+// calc(son, son2, a);
+
+
+// 10 - masala
+
+// function karra(number) {
+
+//     for (let i = 1; i <= 10; i++) {
+
+//         const result = i * number;
+
+//         console.log(`${number} * ${i} = ${result}`);
+//     }
+// }
+// karra(8);
+
+
+
